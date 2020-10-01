644ca9d48a51647ff79b24a91a6dd9054e77b481
Maciej Muszyński <30803273+TheMMaciek@users.noreply.github.com>
2020-10-01 18:04:09 +0200

    Remove not used functionality and clean up the code

--- src/main/scala/org.constellation.cmhotwallet/RequestRunner.scala
+++ src/main/scala/org.constellation.cmhotwallet/RequestRunner.scala
@@ -3,7 +3,7 @@ package org.constellation.cmhotwallet
 import cats.data.EitherT
 import cats.effect.{ConcurrentEffect, Resource, Sync}
 import cats.implicits._
-import org.http4s.{EntityDecoder, Request, Response}
+import org.http4s.{EntityDecoder, Request}
 import org.http4s.client.Client
 
 object RequestRunner {
@@ -12,12 +12,5 @@ object RequestRunner {
   )(client: Resource[F, Client[F]])(implicit d: EntityDecoder[F, A]): EitherT[F, Throwable, A] =
     client.use { c =>
       c.expect[A](request)
-    }.attemptT
-
-  def run[F[_]: Sync: ConcurrentEffect, A, B](request: Request[F], f: Response[F] => F[(A, B)])(
-    client: Resource[F, Client[F]]
-  )(implicit d: EntityDecoder[F, B]): EitherT[F, Throwable, (A, B)] =
-    client.use { c =>
-      c.fetch[(A, B)](request)(f)
     }.attemptT
 }
--- src/main/scala/org.constellation.cmhotwallet/model/config/CliConfig.scala
+++ src/main/scala/org.constellation.cmhotwallet/model/config/CliConfig.scala
@@ -4,17 +4,12 @@ import org.constellation.cmhotwallet.model.CliMethod.CliMethod
 
 case class CliConfig(
   method: CliMethod = null,
-  cmKeystore: String = null,
-  cmAlias: String = null,
-  cmStorepass: Array[Char] = null,
-  cmKeypass: Array[Char] = null,
-  clKeystore: String = null,
-  clAlias: String = null,
-  clStorepass: Array[Char] = null,
-  clKeypass: Array[Char] = null,
+  keystore: String = null,
+  alias: String = null,
+  storepass: Array[Char] = null,
+  keypass: Array[Char] = null,
   loadFromEnvArgs: Boolean = false,
   transferId: Option[Long] = None,
-  pageNr: Int = 1,
   prevTxPath: String = null,
   txPath: String = null
 )
